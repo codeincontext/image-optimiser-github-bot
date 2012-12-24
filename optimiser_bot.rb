162b21e9163f56948cb50958dc6a6051b4c5dd44
Adam Howard <adz@skatty.me>
2012-12-24 03:20:44 +0000

    Get projects to optimise form a redis list

--- optimiser_bot.rb
+++ optimiser_bot.rb
@@ -2,10 +2,27 @@ require 'image_optim'
 require 'find'
 require 'httparty'
 require 'json'
+require 'redis'
 
 require './lib/image_optimiser.rb'
 require './lib/config.rb'
 require './lib/repository.rb'
 
+REDIS = Redis.new
 io = ImageOptimiser.new
-io.optimise_repository 'andrew/24pullrequests'
+
+while true
+  queue, repo = REDIS.blpop "imageoptimiser:queue"
+  next if REDIS.exists "imageoptimiser:success:#{repo}" #or REDIS.exists "imageoptimiser:fail:#{repo}"
+  
+  begin
+    puts "processing #{repo}"
+    io.optimise_repository repo
+    puts "success: #{repo}"
+    REDIS.set "imageoptimiser:success:#{repo}", Time.now.utc
+  rescue
+    puts "fail: #{repo}"
+    puts $!, $@
+    REDIS.set "imageoptimiser:fail:#{repo}", Time.now.utc
+  end
+end
